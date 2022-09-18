f85bae4fb47b48b7bc9f2ec12104bfab891595b7
Forgetting-to-sprinkle-salt-on-meals <85379537+Forgetting-to-sprinkle-salt-on-meals@users.noreply.github.com>
2022-09-18 17:18:39 +0800

    vite init

---
+++ src/test.ts
@@ -0,0 +1,16 @@
+import { Person } from './types'
+
+declare const enum Age {
+  age1 = 18,
+  age2 = 19
+}
+
+let person: Person = {
+  name: 'felix',
+  // age: Age.age1
+}
+
+export {
+  person,
+  // Person
+}
